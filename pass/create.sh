67f94be27b4fbaea051493bac6d96fbd270bfb3b
Zackboy571 <id9799653@gmail.com>
2024-06-08 20:41:42 -0400

    added new files and reformatted the code

--- pass/create.sh
+++ pass/create.sh
@@ -1,29 +1,13 @@
 #!/usr/bin/env bash
 
-check() {
-	local passwd="$1"
-	local passwd_lenght=${#passwd}
-	local upper_check=$(echo "$passwd" | grep -o '[A-Z]' | tr -d '\n' | wc -c)
-	local special_char_check=$(echo "$passwd" | tr -dc '[:punct:]' | wc -c)
-	local num_check=$(echo "$passwd" | grep -o '[0-9]' | tr -d '\n' | wc -c)
-	
-	if [ "$passwd_lenght" -lt "16" ];then
-		echo -e "\nPassword must be a minimum of 16 characters"
-		exit 1
-	fi
-	
-	if [ "$upper_check" -lt "2" ] || [ "$special_char_check" -lt "2" ] || [ "$num_check" -lt "2" ];then
-		echo -e "\nPassword must contain a minimum of 2 upper case letters, numbers and special characters"
-		exit 1
-	fi
+current_path=$(pwd)
 
-}
-
+check=""$current_path"/check.sh"
 
 echo
 echo "Enter your password : " ; read -s passwd
 
-check "$passwd"
+"$check" "$passwd"
 
 echo -e "\nYour password meets the minimum requirements\n"
 
--- pass/user.sh
+++ pass/user.sh
@@ -1,3 +1,31 @@
 #!/usr/bin/env bash
+current_path="$(pwd)"
 
+check=""$current_path"/check.sh"
 
+[ "$UID" -eq 0 ] || exec sudo "$0"
+
+single-user() {
+	read -p "username : " username
+	echo "password : " 
+	read -s password
+	"$check" "$password"
+	echo
+	read -p "preferred shell : " shell
+
+	sudo useradd -m -d /home/"$username" -s /bin/"$shell" "$username"
+
+	echo "$username":"$password" | sudo chpasswd 
+}
+
+multiple-users() {
+	echo "not implemneted yet"	
+}
+
+read -p "Would you like to create (o)ne or (m)ultiple users? : " choice
+
+case "$choice" in
+	"o") single-user ;;
+	"m") multiple-users ;;
+	*) ;;
+esac
